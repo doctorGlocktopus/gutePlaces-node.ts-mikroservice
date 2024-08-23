cb4174582db993c48172f7709109701502b059ca
Fabian Laske <flaske@anche.no>
2024-08-23 15:32:34 +0200

    fixed bug with api calls

--- src/server.ts
+++ src/server.ts
@@ -8,44 +8,46 @@ const app = express();
 const db = connectDB();
 
 async function getCollection() {
-  const db = await connectDB();
+  const db = await connectDB(); // Wait for connection
   const collection = db.connection.db.collection('plz');
   return collection;
 }
 
-
 app.use(express.static(path.join(__dirname, '../public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/api/search',   
  async (req, res) => {
-  const { plz, stadt } = req.query;
-  if (!plz && !stadt) {
-    return res.status(400).json({ error: 'Bitte eine PLZ oder Stadt angeben' });
-  }
+    const { plz, stadt } = req.query;
 
-  try {
-    let query: any = {};
-
-    if (plz) {
-      query.plz = plz;
+    if (!plz && !stadt) {
+        return res.status(400).json({ error: 'Bitte eine PLZ oder Stadt angeben' });
     }
 
-    if (stadt) {
-      query.stadt = new RegExp(stadt as string, 'i');
+    try {
+        let query: any = {};
+
+        console.log(plz, typeof plz)
+
+        if (plz) {
+          query.plz = new RegExp("^" + plz, 'i');
+        }
+
+        if (stadt) {
+          query.stadt = new RegExp("^" + stadt, 'i');
+        }
+
+        const collection = await getCollection();
+        console.log('Query:', query);
+        const results = await collection.find(query).toArray();
+        res.json(results);
+    } catch (err) {
+        console.error('Error fetching data:', err);
+        res.status(500).json({ error: 'Fehler bei der Datenabfrage' });
     }
-
-    const collection = await getCollection();
-
-    const results = await collection.find(query).toArray();
-    res.json(results);
-  } catch (err) {
-    console.error('Error fetching data:', err);
-    res.status(500).json({ error: 'Fehler bei der Datenabfrage' });
-  }
 });
 
 const PORT = process.env.PORT || 5000;
