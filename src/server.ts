50a49d4b6d18346ffb4e28fc181076bbf1ae50b7
Fabian Laske <flaske@anche.no>
2024-08-23 14:51:02 +0200

    added MongoModels

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,52 @@
 import express from 'express';
 import path from 'path';
 import connectDB from './config/db';
+import plzModel from './models/plz-api';
 
 const app = express();
 
-// Verbindung zur Datenbank herstellen
-connectDB();
+const db = connectDB();
 
-// Statische Dateien aus dem 'public' Verzeichnis bereitstellen
+async function getCollection() {
+  const db = await connectDB();
+  const collection = db.connection.db.collection('plz');
+  return collection;
+}
+
+
 app.use(express.static(path.join(__dirname, '../public')));
 
-// Route für die Root-URL
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
-// Beispiel-API-Route
-app.get('/api/plz/search', (req, res) => {
-  res.json({ message: 'API ist erreichbar' });
+app.get('/api/plz/search',   
+ async (req, res) => {
+  const { plz, stadt } = req.query;
+  if (!plz && !stadt) {
+    return res.status(400).json({ error: 'Bitte eine PLZ oder Stadt angeben' });
+  }
+
+  try {
+    let query: any = {};
+
+    if (plz) {
+      query.plz = plz;
+    }
+
+    if (stadt) {
+      query.stadt = new RegExp(stadt as string, 'i');
+    }
+
+    const collection = await getCollection();
+
+    const results = await collection.find(query).toArray();
+    res.json(results);
+  } catch (err) {
+    console.error('Error fetching data:', err);
+    res.status(500).json({ error: 'Fehler bei der Datenabfrage' });
+  }
 });
 
-// Server starten
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
